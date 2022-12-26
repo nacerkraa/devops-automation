769b902095ff719961545dc246fa35b396f2e796
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-12-26 17:39:24 +0100

    Add main files

---
+++ src/main/java/com/example/dockerintgration/DockerIntgrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerintgration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntgrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerIntgrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerintgration/DockerIntgrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerintgration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerIntgrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
